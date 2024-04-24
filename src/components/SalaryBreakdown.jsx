d2a2e221a7d15eabcba8fd0acf22d00875ec8c4d
Mohsin Mir <mirmohsin0001@gmail.com>
2024-04-24 22:31:55 +0530

    added chart

--- src/components/SalaryBreakdown.jsx
+++ src/components/SalaryBreakdown.jsx
@@ -1,5 +1,12 @@
-import React, { useEffect, useRef } from "react";
-import Chart from "chart.js/auto"; // Import all Chart.js defaults
+import React from "react";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  ResponsiveContainer,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 function SalaryBreakdown({ basicSalary, daRate }) {
   const specialPay = 1970;
@@ -15,56 +22,15 @@ function SalaryBreakdown({ basicSalary, daRate }) {
   const nps = (10 / 100) * (basicSalary + DA + specialPay);
   const netSalary = grossSalary - nps;
 
-  // Chart data preparation
-  const chartData = {
-    labels: ["Basic Salary", "SGA", "DA", "HRA", "Special Pay", "TA"],
-    datasets: [
-      {
-        label: "Salary Breakdown",
-        data: [basicSalary, specialGradeAllowance, DA, HRA, specialPay, TA],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)", // Red
-          "rgba(54, 162, 235, 0.2)", // Blue
-          "rgba(255, 206, 86, 0.2)", // Orange
-          "rgba(75, 192, 192, 0.2)", // Grey
-          "rgba(153, 102, 255, 0.2)", // Purple
-          "rgba(255, 159, 64, 0.2)", // Orange-red
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)", // Red
-          "rgba(54, 162, 235, 1)", // Blue
-          "rgba(255, 206, 86, 1)", // Orange
-          "rgba(75, 192, 192, 1)", // Grey
-          "rgba(153, 102, 255, 1)", // Purple
-          "rgba(255, 159, 64, 1)", // Orange-red
-        ],
-
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  // Chart reference
-  const chartRef = useRef(null);
-
-  // Render chart using useEffect
-  useEffect(() => {
-    const myChart = new Chart(chartRef.current, {
-      type: "pie",
-      data: chartData,
-      options: {
-        // Optional options for chart customization (refer to Chart.js documentation)
-        legend: {
-          position: 'right', // Set legend position to 'right'
-        },
-      },
-    });
-
-    // Cleanup function to avoid memory leaks
-    return () => {
-      myChart.destroy();
-    };
-  }, []); // Empty dependency array ensures one-time rendering
+  // Chart data
+  const chartData = [
+    { name: "Basic Salary", value: basicSalary, color: "#0088FE" },
+    { name: "SGA", value: specialGradeAllowance, color: "#00C49F" },
+    { name: "DA", value: DA, color: "#9966FF" },
+    { name: "HRA", value: HRA, color: "#FF8042" },
+    { name: "Special Pay", value: specialPay, color: "#FF6666" },
+    { name: "TA", value: TA, color: "#FFBB28" },
+  ];
 
   return (
     <>
@@ -107,11 +73,43 @@ function SalaryBreakdown({ basicSalary, daRate }) {
         </div>
       </div>
 
-      <div className="w-75 m-auto">
-        <canvas ref={chartRef} width="400" height="400"></canvas>
-      </div>
+      <div className="my-5 ">
+        <ResponsiveContainer width="100%" height={250}>
+          <PieChart>
+            <Pie
+              data={chartData}
+              dataKey="value"
+              nameKey="name"
+              cx="50%"
+              cy="50%"
+              outerRadius={60}
+              fill="#8884d8"
+              // labelLine={false}
+              paddingAngle={5}
+              innerRadius={50}
+              label={(entry) => entry.value.toFixed(0)}
+            >
+              {chartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={entry.color} />
+              ))}
+            </Pie>
 
-      <div className="mt-5 pb-5">
+            <Tooltip formatter={(value) => value.toFixed(0)} />
+
+            <Legend
+              align="center"
+              verticalAlign="bottom"
+              layout="horizontal"
+              iconSize={10}
+              wrapperStyle={{ fontSize: "10px" }}
+              formatter={(value) => (
+                <span style={{ color: "black" }}>{value}</span>
+              )}
+            />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+      <div className="mt-0 pb-5">
         <p className="text-muted fst-italic pb-5">
           DA calculated @ {(daRate * 100).toFixed(2)}%, HRA @ 10.25%, SGA @
           26.5% and NPS @ 10% of (Basic + DA)
